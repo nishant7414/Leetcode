aa3523fddf59e82e2b869710f7e7c0dd5448dd74
NISHANT KUMAR <nishantkumar7414@gmail.com>
2026-05-26 14:00:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (98.14%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    static int numberOfSpecialChars(string& word) {
+        unsigned A[2];
+        for(char c: word){
+            bool isLower=c&(1<<5);
+            A[isLower]|=(1<<(c&31));
+        }
+        return popcount(A[0]&A[1]);
+    }
+};
